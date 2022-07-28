5d857df221befc09349e88ebdab6e454594804bb
haema <haemahmtea@gmail.com>
2022-07-29 04:05:35 +0900

    README.md 수정. 기초세팅 next_step.zip 프로젝트 최종 수정.

--- src/main/java/com/example/next_step/HelloController.java
+++ src/main/java/com/example/next_step/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.next_step;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @Controller
 public class HelloController {
 
--- src/main/java/com/example/next_step/user/UserController.java
+++ src/main/java/com/example/next_step/user/UserController.java
@@ -1,10 +1,11 @@
 package com.example.next_step.user;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/user")
 public class UserController {
